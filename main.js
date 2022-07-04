5ef3da2dd9b2d190785df5728a2d2e89e57644cc
basitali111 <allahbadshah148@gmail.com>
2022-07-04 22:14:46 +0500

    adding some basci structure

---
+++ main.js
@@ -0,0 +1,11 @@
+
+class book {
+constructor(author,title){
+    this.book= author;
+    this.title = title;
+    function Book(){
+console.log("the book name is ${this.book} and title is ${this.tile} ");
+    }
+    
+}
+}
