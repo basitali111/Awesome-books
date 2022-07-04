72706321e95a76669a8965d29a8d851a6d743bb4
basitali111 <allahbadshah148@gmail.com>
2022-07-05 01:48:12 +0500

    adding javascript add

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-
-class book {
-constructor(author,title){
-    this.book= author;
-    this.title = title;
-    function Book(){
-console.log("the book name is ${this.book} and title is ${this.tile} ");
-    }
-    
+console.log("string");
+function Book(author, title) {
+  this.book = author;
+  this.title = title;
 }
+function Display() {
+Display.prototype.add = function (book) {
+  console.log("adding to ui");
+ let tablebody = document.getElementById("tablebody");
+  let uistring = `
+                 <tr>
+                      <td>${book.author}</td>
+                      <td>${book.title}</td>
+                 </tr>
+
+`;
+tablebody.innerHTMl +=uistring;
+};
+}
+// Display.prototype.clear = function () {
+//   let libraryForm = document.getElementById("librarayForm");
+//   libraryForm.reset();
+// };
+
+// adding event listner to form to get author and title from form
+document
+  .getElementById("addbutton")
+  .addEventListener("click", libraryFormSubmit);
+function libraryFormSubmit(e) {
+  console.log("you have submitted library form");
+  let author = document.getElementById("authorname").value;
+  let title = document.getElementById("bookname").value;
+  let book = new Book(author, title);
+  console.log(book);
+  let display = new Display();
+  display.add(book);
+//   display.clear();
+  e.preventDefault();
 }
